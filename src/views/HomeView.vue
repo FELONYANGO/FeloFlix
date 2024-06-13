272599ef18f06ffe2258bdf742b2b7f9b86f6b11
FELONYANGO <felixonyango951@gmail.com>
2024-06-13 13:09:55 +0300

    added the TMDB Api fetch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,71 @@
-<script setup>
-
-
-</script>
-
 <template>
-
-  <p>HOME</p>
-
- 
+  <div class="">
+    <swiper :slides-per-view="4" :space-between="10" :pagination >
+      <swiper-slide v-for="item in fetchedData.results" :key="item.id">
+        <img
+          :src="getFullBackdropPath(item.backdrop_path)"
+          alt="Backdrop Image"
+          v-if="item.backdrop_path"
+        />
+        <p class="">{{ getTitle(item) }}</p>
+        <p>{{ item.id }}</p>
+      </swiper-slide>
+    </swiper>
+  </div>
 </template>
+
+<script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/swiper-bundle.css';
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  data() {
+    return {
+      API_BASE: import.meta.env.VITE_APP_API_URL,
+      API_KEY: import.meta.env.VITE_APP_API_KEY,
+      API_URL: '',
+      fetchedData: null,
+    };
+  },
+  created() {
+    this.API_URL = `${this.API_BASE}trending/all/day?language=en-US&api_key=${this.API_KEY}`;
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await fetch(this.API_URL, {
+          headers: {
+            'Authorization': `Bearer ${this.API_KEY}`
+          }
+        });
+
+        // Check if the response is JSON
+        const contentType = response.headers.get('content-type');
+        if (contentType && contentType.indexOf('application/json') !== -1) {
+          const data = await response.json();
+          this.fetchedData = data;
+        } else {
+          // If not JSON, log the response text for debugging
+          const text = await response.text();
+          console.error('Response is not JSON:', text);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    },
+    // get title
+    getTitle(item) {
+      return item.title || item.name || item.original_title || item.original_name;
+    },
+    // get buckdroppath
+    getFullBackdropPath(backdropPath) {
+      return `https://image.tmdb.org/t/p/w300${backdropPath}`;
+    },
+  },
+};
+</script>
--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,9 +1,28 @@
-<script>
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Hello World!')
+// method to get all movies from the API
+const getMovies = async () => {
+    try {
+        const response = await fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=ac13cacbd635a127599a00e738323997')
+        const data = await response.json()
+        
+        console.log(data)  
+        return data 
+    } catch (error) {
+        console.error(error)
+    }
+}
+const movies= getMovies()
 </script>
 <template>
-    <div class="about">
+    
         <h1>this is the movies page</h1>
-    </div>
+        <p>{{ message }}</p>
+        <p>{{ movies }}</p>
+        <button @click="getMovies">Get Movies</button>
+    
 </template>
 <style scoped>
 
